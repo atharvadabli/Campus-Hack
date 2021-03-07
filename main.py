2d251cf7c8521905cec7446ca21bd0853e205e80
Kushagra-Rode27 <79641134+Kushagra-Rode27@users.noreply.github.com>
2021-03-07 17:20:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,60 +3,106 @@ import requests
 import json
 from weather import *
 from discord.ext.commands import Bot
-token = 'ODE3Njc5NDc2OTM3NzE5ODI4.YENBWw.YXCPTCNB5__i02YI8SnFmiEcwFU'
+
+token = 'ODE3NjQ1NjQxNDgzODEyODk1.YEMh2A.gOqlDXhEqM97PU5HkBoDJOSb65I'
 api_key = '3dd56669bb9854ffe5fbacaa2818f7f9'
+api_key2 = 'm17o2uh8c626ljgjuhj9kud0pp'
+api_key3 = 'aae6dc1c35f948b0a66b1b6bb07ac76d'
 client = discord.Client()
-command_prefix = 'w.'
-command_prefix2 = 'aqi.'
-command_prefix3 = 'val.aqi'
+command_prefix1 = 'mausam.'
+command_prefix2 = 'forecast8days.' 
+command_prefix3 = 'forecast.'
+command_prefix4 = 'aqi.'
+command_prefix6 = 'alerts.'
 @client.event
 async def on_ready():
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='w.[location]'))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='aqi.[location]'))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='val.aqi'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='mausam.[location]'))
+    
 @client.event
 async def on_message(message):
-    if message.author != client.user and message.content.startswith(command_prefix):
-        if len(message.content.replace(command_prefix, '')) >= 1:
-            location = message.content.replace(command_prefix, '').lower()
+    if message.author != client.user and message.content.startswith(command_prefix1):
+        if len(message.content.replace(command_prefix1, '')) >= 1:
+            location = message.content.replace(command_prefix1, '').lower()
             url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
             try:
-                data1 = (json.loads(requests.get(url).content)['main'])
+                data1 = parse_data1(json.loads(requests.get(url).content)['main'])
                 data2 = (json.loads(requests.get(url).content)['visibility'])
-                latitude = (json.loads(requests.get(url).content)['coord']['lat'])
-                longitude = (json.loads(requests.get(url).content)['coord']['lon'])
-                url2 = f'http://api.airpollutionapi.com/1.0/aqi?lat={latitude}&lon={longitude}&APPID=m17o2uh8c626ljgjuhj9kud0pp'
+                data3 = parse_data2(json.loads(requests.get(url).content)['weather'][0])
+                
+                coord = (json.loads(requests.get(url).content)['coord'])
+                url2 = f'http://api.airpollutionapi.com/1.0/aqi?lat={coord["lat"]}&lon={coord["lon"]}&APPID={api_key2}'
                 alert = (json.loads(requests.get(url2).content)['data']['alert'])
                 value = (json.loads(requests.get(url2).content)['data']['value'])
                 pm25 = (json.loads(requests.get(url2).content)['data']['aqiParams'][1]['aqi'])
                 pm10  = (json.loads(requests.get(url2).content)['data']['aqiParams'][2]['aqi'])
                 aqi = [value, alert, pm25, pm10]
-                await message.channel.send(embed=weather_message(data1,data2,location,aqi))
+                
+                
+                
+                await message.channel.send(embed=weather_message(data1,data2,data3,aqi,location))
+                
             except KeyError:
                 await message.channel.send(embed=error_message(location))
-
-
-    if message.author != client.user and message.content.startswith(command_prefix2):
+    elif message.author != client.user and message.content.startswith(command_prefix2):
         if len(message.content.replace(command_prefix2, '')) >= 1:
             location = message.content.replace(command_prefix2, '').lower()
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric' 
+            try:
+                data4 = (json.loads(requests.get(url).content)['coord'])
+                url3 = f'https://api.openweathermap.org/data/2.5/onecall?lat={data4["lat"]}&lon={data4["lon"]}&exclude=current,minutely,hourly,alerts&appid={api_key}&units=metric'
+                data5 = (json.loads(requests.get(url3).content)['daily'])
+                await message.channel.send(embed=weather_message2(data5,location))
+            except KeyError:
+                await message.channel.send(embed=error_message(location))
+    elif message.author!=client.user and message.content.startswith(command_prefix3):
+        if len(message.content.replace(command_prefix3, '')) >= 1:
+                date=''
+                inp=message.content.replace(command_prefix3,'')
+                for i in range(len(message.content.replace(command_prefix3,''))):
+                    if i==10:
+                        break
+                    else:
+                        date=date+inp[i]
+                location = ''
+                for i in range(10,len(message.content.replace(command_prefix3,''))):
+                    location=location+inp[i]
+                    location.lower()
+                url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
+                try:
+                    coord = (json.loads(requests.get(url).content)['coord'])
+                    url4=f'https://api.openweathermap.org/data/2.5/onecall?lat={coord["lat"]}&lon={coord["lon"]}&appid={api_key}&units=metric'
+                    data6 = (json.loads(requests.get(url4).content)['daily'])
+                    for i in range(len(data6)):
+                        if f(date)==data6[i]['dt']:
+                            data7=data6[i]
+                            await message.channel.send(embed=weather_message3(data7,date,location))
+                except KeyError:
+                    await message.channel.send(embed=error_message2(location,date))
+    elif message.author != client.user and message.content.startswith(command_prefix4):
+        if len(message.content.replace(command_prefix4, '')) >= 1:
+            location = message.content.replace(command_prefix4, '').lower()
             url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
             try:
-                latitude = (json.loads(requests.get(url).content)['coord']['lat'])
-                longitude = (json.loads(requests.get(url).content)['coord']['lon'])
-                url2 = f'http://api.airpollutionapi.com/1.0/aqi?lat={latitude}&lon={longitude}&APPID=m17o2uh8c626ljgjuhj9kud0pp'
+                coord = (json.loads(requests.get(url).content)['coord'])
+                url2 = f'http://api.airpollutionapi.com/1.0/aqi?lat={coord["lat"]}&lon={coord["lon"]}&APPID={api_key2}'
                 alert = (json.loads(requests.get(url2).content)['data']['alert'])
                 value = (json.loads(requests.get(url2).content)['data']['value'])
                 pm25 = (json.loads(requests.get(url2).content)['data']['aqiParams'][1]['aqi'])
                 pm10  = (json.loads(requests.get(url2).content)['data']['aqiParams'][2]['aqi'])
                 aqi = [value, alert, pm25, pm10]
-                await message.channel.send(embed=weather_message2(location,aqi))
+                await message.channel.send(embed=weather_message4(location,aqi))
             except KeyError:
-                await message.channel.send(embed=error_message(location))           
+                await message.channel.send(embed=error_message(location))    
     
-
-    if message.author != client.user and message.content.startswith(command_prefix3):
-        
-        await message.channel.send(embed = weather_message3())
-        
-
+    elif message.author != client.user and message.content.startswith(command_prefix6):
+        if len(message.content.replace(command_prefix6, '')) >= 1:
+            location = message.content.replace(command_prefix6, '').lower()
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
+            try:
+                coord = (json.loads(requests.get(url).content)['coord']) 
+                url5 = f'https://api.weatherbit.io/v2.0/alerts?lat={coord["lat"]}&lon={coord["lon"]}&key={api_key3}' 
+                data8 = (json.loads(requests.get(url5).content)['alerts'])
+                await message.channel.send(embed=weather_message6(data8,location))
+            except KeyError:
+                await message.channel.send(embed=error_message(location))    
 client.run(token)
