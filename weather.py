ba06f7f43ecbc7611b66633ab227869e9162ebd0
Kushagra-Rode27 <79641134+Kushagra-Rode27@users.noreply.github.com>
2021-03-07 17:21:07 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,6 @@
 import discord
+import datetime 
+import time
 
 color = 0xFF6500
 key_features = {
@@ -6,18 +8,37 @@ key_features = {
     'feels_like' : 'Feels Like(Celsius)',
     'temp_min' : 'Minimum Temperature(Celsius)',
     'temp_max' : 'Maximum Temperature(Celsius)',
-    'humidity' : 'humidity(%)',
-    'pressure' : 'Pressure(hPa)'
+    'humidity' : 'Humidity(%)'
 }
+weath = {'description': 'Description','icon':'icon'}
+key_features2 = {
+    "day": 'Temperature(Celsius)',
+    "min": 'Minimum Temperature(Celsius)',
+    "max": 'Maximum Temperature(Celsius)',
+    "night":'Night Temp.(C)',
+    "eve": 'Evening Temp. (C)',
+    "morn": 'Morning Temp. (C)'
+}
+key_features3={
+    'event':'Event',
+    'description':'Description of event:'
+}
+def f(date):
+    dt = datetime.datetime(int(date[6]+date[7]+date[8]+date[9]),int(date[3]+date[4]),int(date[0]+date[1]),12,0)
+    return int(time.mktime(dt.timetuple()))
 
-
-def parse_data(data):
-    # del data['humidity']
-    # del data['pressure']S
+def parse_data1(data):
+    
+    del data['pressure']
+    return data
+def parse_data2(data):
+    del data['main']
+    del data['id']
+    
     return data
 
 
-def weather_message(data1,data2,location, aqi):
+def weather_message(data1,data2,data3,aqi,location):
     location = location.title()
     message = discord.Embed(
         title=f'{location} Weather',
@@ -34,17 +55,97 @@ def weather_message(data1,data2,location, aqi):
     message.add_field(
             name= "Visibility(metres)",
             value=str(data2),
-            inline=False
-        )
+            inline=False)
+    
+    
+    
+    for key in data3 :
+        if key == 'description':
+            message.add_field(
+                name=weath[key],
+                value=(data3[key]),
+                inline=False
+            )
+        elif key == 'icon':
+            ur = f'http://openweathermap.org/img/w/{data3[key]}.png'
+            message.set_thumbnail(url= ur)
+    
     message.add_field(
             name= "AQI",
             value=str(aqi[0]),
             inline=False
         ) 
+    
     return message
 
 
-def weather_message2(location, aqi):
+def weather_message2(data5,location):
+    location = location.title()
+    message = discord.Embed(
+        title=f' 8- Day Weather for {location}',
+        description=f'Here is the forecasted weather in {location}.',
+        color=color
+    )
+    for i in range(8):
+        
+        for key in data5[i]:
+            if key == 'dt':
+                from datetime import datetime
+                t = data5[i][key]
+                dt_obj = datetime.fromtimestamp(t).strftime('%d-%m-%y') 
+                message.add_field(
+                name='Date',
+                value=(dt_obj),
+                inline=False
+            )
+            
+
+            elif key == 'temp':
+                message.add_field(
+                name='Temperature',
+                value=(data5[i][key]['day']),
+                inline=False
+            )
+            elif key == 'weather':
+                
+                
+                message.add_field(
+                name='Description',
+                value=(data5[i][key][0]['description']),
+                inline=False)
+                
+    return message
+
+
+def weather_message3(data7,date,location):
+    
+    location = location.title()
+    message = discord.Embed(
+        title=f'{location} Weather',
+        description=f'Here is the weather in {location} on {date}.',
+        color=color
+    )
+    for key in data7['temp']:
+        message.add_field(
+            name=key_features2[key],
+            value=str(data7['temp'][key]),
+            inline=False
+        )
+    message.add_field(
+            name= "Humidity",
+            value=str(data7['humidity']),
+            inline=False
+    )
+    message.add_field(
+            name= "Description",
+            value=str(data7['weather'][0]['description']),
+            inline=False
+    )
+    ur = f'http://openweathermap.org/img/w/{data7["weather"][0]["icon"]}.png'
+    message.set_thumbnail(url= ur)
+    return message
+
+def weather_message4(location,aqi):
     location = location.title()
     message = discord.Embed(
         title=f'{location} AQI',
@@ -74,53 +175,50 @@ def weather_message2(location, aqi):
         )
     
     return message
-
-
-def weather_message3():
-    message = discord.Embed(
-        title='AQI levels',
-        description='Here is the Air Quality Levels.',
+def weather_message6(data8,location):
+    location = location.title()
+    if data8 == []:
+        return discord.Embed(
+        title='No Alerts',
+        description=f'There are no weather alerts for {location}.',
         color=color
     )
-    message.add_field(
-            name= "0-50",
-            value= 'Good',
-            inline=False
+    else:
+        message = discord.Embed(
+            title=f'{location} Weather Alerts',
+            description=f'Here are the weather alerts in {location}.',
+            color=color
         )
-    
-    message.add_field(
-            name= "50-100",
-            value="Moderate",
-            inline=False
+        message.add_field(
+            name="Title",
+            value= data8[0]['title'],
+            color = color
         )
-    message.add_field(
-            name= "101-150",
-            value="Unhealthy for sensitive groups",
-            inline=False
+        message.add_field(
+            name="Description",
+            value=data8[0]['description'],
+            color = color
         )
-    message.add_field(
-            name= "150-200",
-            value= "Unhealthy",
-            inline=False
+        message.add_field(
+            name="Severity",
+            value=data8[0]['severity'],
+            color = color
         )
-    
-    message.add_field(
-            name= "201-300",
-            value= "Very Unhealthy",
-            inline=False
-        )
+        
 
-    message.add_field(
-            name= "301-500",
-            value="Hazardous",
-            inline=False
-        )
     
-    return message
+
 def error_message(location):
     location = location.title()
     return discord.Embed(
         title='Error',
         description=f'There was an error retrieving weather data for {location}.',
+        color=color
+    )
+def error_message2(location,date):
+    location = location.title()
+    return discord.Embed(
+        title='Error',
+        description=f'There was an error retrieving weather data for {location} on {date}.',
         color=color
     )
